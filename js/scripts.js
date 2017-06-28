17e6c96331ff60e3146629d3652f44d0fb754125
Robert Graupner <robertgraupner@air-robert.home>
2017-06-28 22:58:58 +0200

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-var a = 2;
-var h = 3;
 function getTriangleArea(a, h) {
-	if (a>0 && h>0) {
-	return a*h/2
+	if (a > 0 && h > 0) {
+		return a*h/2
 	} else {
-	return console.error('nieprawidłowe dane');
+		return console.error('nieprawidłowe dane');
 	}
 }
 var triangle1Area = getTriangleArea(10, -15);
 console.log(triangle1Area);
-var triangle2Area = getTriangleArea(2, 3);
+var triangle2Area = getTriangleArea(8, 3);
 console.log(triangle2Area);
 var triangle3Area = getTriangleArea(20, 21);
 console.log(triangle3Area);
