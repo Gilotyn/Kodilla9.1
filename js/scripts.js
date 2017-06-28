8c7af1a08cab5d32237de61aacbdf433f593cb69
Robert Graupner <robertgraupner@air-robert.home>
2017-06-28 19:43:33 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var a = 2;
+var h = 3;
+function getTriangleArea(a, h) {
+	if (a>0 && h>0) {
+	return a*h/2
+	} else {
+	return console.error('nieprawidłowe dane');
+	}
+}
+var triangle1Area = getTriangleArea(10, -15);
+console.log(triangle1Area);
+var triangle2Area = getTriangleArea(2, 3);
+console.log(triangle2Area);
+var triangle3Area = getTriangleArea(20, 21);
+console.log(triangle3Area);
